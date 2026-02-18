46c33741e54f06debb33a9bef9008f27b68f3c8a
chocho77 <chavmom@gmail.com>
2026-02-18 23:00:57 +0200

    add profile.ts

--- src/app/pages/profile/profile.ts
+++ src/app/pages/profile/profile.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
-  templateUrl: './profile.html',
-  styleUrl: './profile.css',
-})
-export class Profile {
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  template: `
+    <h2>Профил на потребител #{{ id() }}</h2>
+    <p>Тук можеш да четеш от ActivatedRoute или директно от input :id</p>
 
+    <a routerLink="/home">Начало</a>
+  `
+})
+export class Profile{
+  private route = inject(ActivatedRoute);
+
+  // Начин 1: стар, но работи
+  id = this.route.snapshot.paramMap.get('id');
+
+  // Начин 2: модерен (от v17+), с input
+  // id = input<string>();   // и в routes: { path: 'profile/:id', component: ProfileComponent, data: { title: 'Профил' } }
 }
