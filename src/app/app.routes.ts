769c8f12329b9ef0a643c6d260b8d0fde397b5c8
chocho77 <chavmom@gmail.com>
2026-02-18 22:34:44 +0200

    New routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Profile } from './pages/profile/profile';
+import { Settings } from './pages/settings/settings';
+import { Result } from './pages/result/result';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'about', component: About },
+
+  // Пример с route param :id
+  { path: 'profile/:id', component: Profile },
+
+  // Пример с query параметри + service
+  { path: 'settings', component: Settings },
+
+  // Където показваме събраните данни
+  { path: 'result', component: Result },
+
+  { path: '**', redirectTo: 'home' }
+];
+
